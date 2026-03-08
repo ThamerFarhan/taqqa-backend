ce423e2b96679dcb846b6382ed51f944c97463fd
ThamerFarhan <ooiyt980@gmail.com>
2026-03-08 13:42:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 // ===================== MIDDLEWARE =====================
 app.use(helmet());
 app.use(cors({
